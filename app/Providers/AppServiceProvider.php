2d22fc7604ecfd769a6db31dd7f2e32d3b5c6b03
Taylor, Daniel <daniel.taylor@nuance.com>
2022-11-17 23:04:43 +0000

    Add Wonde Client as singleton to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Wonde\Client as WondeClient;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(WondeClient::class, function () {
+            $client = new WondeClient(config('wonde.api_token'));
+            return $client;
+        });
     }
 
     /**
