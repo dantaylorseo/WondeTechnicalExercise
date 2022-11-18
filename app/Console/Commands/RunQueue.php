18b7d9b05d6c62b375e0b421d415eb1cfa7ff78c
Taylor, Daniel <daniel.taylor@nuance.com>
2022-11-18 13:04:16 +0000

    Add new methids to create data

--- app/Console/Commands/RunQueue.php
+++ app/Console/Commands/RunQueue.php
@@ -2,10 +2,12 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Employee;
 use App\Models\User;
-use Wonde\Client as WondeClient;
+use App\Models\Student;
+use App\Models\Employee;
+use App\Models\WondeClass;
 use Illuminate\Console\Command;
+use Wonde\Client as WondeClient;
 use Illuminate\Support\Facades\Validator;
 
 class RunQueue extends Command
@@ -160,8 +162,100 @@ class RunQueue extends Command
                 ],
                 $validator->validated()
             );
+            $this->_saveLessonsForClass( $class->id);
         });
 
         $this->info('Classes saved');
+    }
+
+    private function _saveStudentsForClass( $students, $class_id ) {
+        collect($students)->each(function ($student) use ($class_id) {
+            $validator = Validator::make(
+                [
+                    'id' => $student->id,
+                    'upi' => $student->upi,
+                    'mis_id' => $student->mis_id,
+                    'initials' => $student->initials,
+                    'surname' => $student->surname,
+                    'forename' => $student->forename,
+                    'middle_names' => $student->middle_names,
+                    'legal_surname' => $student->legal_surname,
+                    'legal_forename' => $student->legal_forename,
+                ],
+                [
+                    'id' => 'required|string',
+                    'upi' => 'required|string',
+                    'mis_id' => 'required|string',
+                    'initials' => 'nullable|string',
+                    'surname' => 'required|string',
+                    'forename' => 'nullable|string',
+                    'middle_names' => 'nullable|string',
+                    'legal_surname' => 'nullable|string',
+                    'legal_forename' => 'nullable|string',
+                ]
+            );
+
+            if ($validator->fails()) {
+                $this->error('Validation failed');
+                $this->error($validator->errors());
+                return;
+            }
+
+            $createdStudent = Student::updateOrCreate(
+                [
+                    'id' => $student->id,
+                ],
+                $validator->validated()
+            );
+            $createdStudent->classes()->attach($class_id);
+        });
+        $this->info('Students saved');
+    }
+
+    private function _saveLessonsForClass($class_id) {
+        $class = $this->school->classes->get($class_id, [
+            'lessons,students'
+        ]);
+
+        $this->_saveStudentsForClass( $class->students->data ?? [], $class->id );
+
+        collect($class->lessons->data ?? [])->each(function ($lesson) use ($class, $class_id) {
+            $validator = Validator::make(
+                [
+                    'id' => $lesson->id,
+                    'room' => $lesson->room,
+                    'period' => $lesson->period,
+                    'period_instance_id' => $lesson->period_instance_id,
+                    'employee_id' => $lesson->employee,
+                    'alternative' => $lesson->alternative,
+                    'start_at' => $lesson->start_at->date,
+                    'end_at' => $lesson->end_at->date,
+                ],
+                [
+                    'id' => 'required|string',
+                    'room' => 'nullable|string',
+                    'period' => 'nullable|string',
+                    'period_instance_id' => 'nullable|integer',
+                    'employee_id' => 'nullable|string',
+                    'alternative' => 'nullable|boolean',
+                    'start_at' => 'required|date',
+                    'end_at' => 'required|date',
+                ]
+            );
+            
+            if ($validator->fails()) {
+                $this->error('Validation failed');
+                $this->error($validator->errors());
+                return;
+            }
+            
+            $dbClass = WondeClass::find($class_id);
+            $dbClass->lessons()->updateOrCreate(
+                [
+                    'id' => $lesson->id,
+                ],
+                $validator->validated()
+            );
+        });
     }
 }
